60fa8440c63f3264ee9b72ced5fe0b2c11a25f20
want-r-u-trying-to-do <168757947+want-r-u-trying-to-do@users.noreply.github.com>
2024-05-02 23:39:17 +0800

    Add files via upload

--- toEmail.js
+++ toEmail.js
@@ -1,10 +1,12 @@
 function emailSend(){
 
-	var userName = document.getElementById('location').value;
-	// var phone = document.getElementById('phone').value;
-	// var email = document.getElementById('email').value;
+	var userName = document.getElementById('name').value;
+	var phone = document.getElementById('phone').value;
+	var email = document.getElementById('email').value;
 
-	var messageBody = "Name " + userName;
+	var messageBody = "Name " + userName +
+	"<br/> Phone " + phone +
+	"<br/> Email " + email;
 	Email.send({
     Host : "smtp.elasticemail.com",
     Username : "nima96981@gmail.com",
@@ -15,6 +17,12 @@ function emailSend(){
     Body : messageBody
 }).then(
   message => {
+  	if(message=='OK'){
+  		swal("Secussful", "You clicked the button!", "success");
+  	}
+  	else{
+  		swal("Error", "You clicked the button!", "error");
+  	}
   }
 );
 }
